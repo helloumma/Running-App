7f4124b42e46319716acb1cbd69b8082af033bfd
756D6D61 <756D6D61@gmail.com>
2019-09-05 15:24:45 +0100

    Existing Routine style changes.

--- components/ExistingScreen.js
+++ components/ExistingScreen.js
@@ -5,7 +5,8 @@ export default class Existingscreen extends Component {
   render() {
     return (
         <Container style={{flex: 1,backgroundColor: '#A2FFAA', justifyContent: 'center'}}>
-        <Text style={{fontSize:90, textAlign: 'center', color:'#00C7AF', fontWeight: 'bold'}}>Existing Routines</Text>
+        <Text style={{fontSize:40, textAlign: 'center', color:'#00C7AF', fontWeight: 'bold'}}>START THAT RUN</Text>
+        <Text style={{fontSize:40, textAlign: 'center', color:'#07B162', fontWeight: 'bold'}}>Existing Routines</Text>
         <Content>
             <Button style={{padding:'35%', backgroundColor:'#07B162'}}>
                 <Text style={{fontSize:20, textAlign: 'center', color:'white', fontWeight: 'bold'}}>Routine</Text>
