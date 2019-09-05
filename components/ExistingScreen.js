770fbb7463d695da061c1eed1f5767c42113bd31
756D6D61 <756D6D61@gmail.com>
2019-09-05 15:22:08 +0100

    Bug fixing.

--- components/ExistingScreen.js
+++ components/ExistingScreen.js
@@ -5,31 +5,31 @@ export default class Existingscreen extends Component {
   render() {
     return (
         <Container style={{flex: 1,backgroundColor: '#A2FFAA', justifyContent: 'center'}}>
-        <Text style={{fontSize:'90%', textAlign: 'center', color:'#00C7AF', fontWeight: 'bold'}}>Existing Routines</Text>
+        <Text style={{fontSize:90, textAlign: 'center', color:'#00C7AF', fontWeight: 'bold'}}>Existing Routines</Text>
         <Content>
             <Button style={{padding:'35%', backgroundColor:'#07B162'}}>
-                <Text style={{fontSize:'20%', textAlign: 'center', color:'white', fontWeight: 'bold'}}>Routine</Text>
+                <Text style={{fontSize:20, textAlign: 'center', color:'white', fontWeight: 'bold'}}>Routine</Text>
             </Button>
             <Button style={{padding:'35%', backgroundColor:'#07B162'}}>
-                <Text style={{fontSize:'20%', textAlign: 'center', color:'white', fontWeight: 'bold'}}>Routine</Text>
+                <Text style={{fontSize:20, textAlign: 'center', color:'white', fontWeight: 'bold'}}>Routine</Text>
             </Button>
             <Button style={{padding:'35%', backgroundColor:'#07B162'}}>
-                <Text style={{fontSize:'20%', textAlign: 'center', color:'white', fontWeight: 'bold'}}>Routine</Text>
+                <Text style={{fontSize:20, textAlign: 'center', color:'white', fontWeight: 'bold'}}>Routine</Text>
             </Button>
             <Button style={{padding:'35%', backgroundColor:'#07B162'}}>
-                <Text style={{fontSize:'20%', textAlign: 'center', color:'white', fontWeight: 'bold'}}>Routine</Text>
+                <Text style={{fontSize:20, textAlign: 'center', color:'white', fontWeight: 'bold'}}>Routine</Text>
             </Button>
             <Button style={{padding:'35%', backgroundColor:'#07B162'}}>
-                <Text style={{fontSize:'20%', textAlign: 'center', color:'white', fontWeight: 'bold'}}>Routine</Text>
+                <Text style={{fontSize:20, textAlign: 'center', color:'white', fontWeight: 'bold'}}>Routine</Text>
             </Button>
             <Button style={{padding:'35%', backgroundColor:'#07B162'}}>
-                <Text style={{fontSize:'20%', textAlign: 'center', color:'white', fontWeight: 'bold'}}>Routine</Text>
+                <Text style={{fontSize:20, textAlign: 'center', color:'white', fontWeight: 'bold'}}>Routine</Text>
             </Button>
             <Button style={{padding:'35%', backgroundColor:'#07B162'}}>
-                <Text style={{fontSize:'20%', textAlign: 'center', color:'white', fontWeight: 'bold'}}>Routine</Text>
+                <Text style={{fontSize:20, textAlign: 'center', color:'white', fontWeight: 'bold'}}>Routine</Text>
             </Button>
             <Button style={{padding:'35%', backgroundColor:'#07B162'}}>
-                <Text style={{fontSize:'20%', textAlign: 'center', color:'white', fontWeight: 'bold'}}>Routine</Text>
+                <Text style={{fontSize:20, textAlign: 'center', color:'white', fontWeight: 'bold'}}>Routine</Text>
             </Button>
         </Content>
     </Container>
--- components/HomeScreen.js
+++ components/HomeScreen.js
@@ -7,18 +7,18 @@ export default class Homescreen extends Component {
     //style: PropTypes.oneOfType([PropTypes.object, PropTypes.number])
     return (
         <Container style={{flex: 1,backgroundColor: '#A2FFAA', justifyContent: 'center'}}>
-            <Text style={{fontSize:'100%', textAlign: 'center', color:'#00C7AF', fontWeight: 'bold'}}>START THAT RUN</Text>
+            <Text style={{fontSize:100, textAlign: 'center', color:'#00C7AF', fontWeight: 'bold'}}>START THAT RUN</Text>
             <Content>
                 <Button style={{padding:'30%', backgroundColor:'#07B162'}} onPress={() => this.props.navigation.navigate('New')}>
-                    <Text style={{fontSize:'20%', textAlign: 'center', color:'white', fontWeight: 'bold'}}>New Routine</Text>
+                    <Text style={{fontSize: 20, textAlign: 'center', color:'white', fontWeight: 'bold'}}>New Routine</Text>
                 </Button>
                 
                 <Button style={{padding:'25%', backgroundColor:'#07B162'}} onPress={() => this.props.navigation.navigate('Existing')}>
-                    <Text style={{fontSize:'20%', textAlign: 'center', color:'white', fontWeight: 'bold'}}>Existing Routine</Text>
+                    <Text style={{fontSize:20, textAlign: 'center', color:'white', fontWeight: 'bold'}}>Existing Routine</Text>
                 </Button>
 
                 <Button style={{padding:'30%', backgroundColor:'#07B162'}} onPress={() => this.props.navigation.navigate('Previous')}>
-                    <Text style={{fontSize:'20%', textAlign: 'center', color:'white', fontWeight: 'bold'}}>Previous Runs</Text>
+                    <Text style={{fontSize:20, textAlign: 'center', color:'white', fontWeight: 'bold'}}>Previous Runs</Text>
                 </Button>
             </Content>
         </Container>
