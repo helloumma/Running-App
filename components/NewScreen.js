39566f46d594ccb4b0a89fdb4aa085b68ce652b8
756D6D61 <756D6D61@gmail.com>
2019-09-05 12:39:28 +0100

    Basic form added to new routine screen.

--- components/NewScreen.js
+++ components/NewScreen.js
@@ -1,12 +1,56 @@
 import React, { Component } from 'react';
-import { Button, View, Text } from 'react-native';
+import { Container, Header, Content, Form, Item, Input, Label, Footer, FooterTab, Button, Icon } from 'native-base';
 
 export default class Newscreen extends Component {
   render() {
     return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>New Screen</Text>
-      </View>
+        <Container>
+         <Header />
+        <Content>
+          <Form>
+            <Item floatingLabel>
+              <Label>Name</Label>
+              <Input />
+            </Item>
+            <Item floatingLabel>
+              <Label>Time</Label>
+              <Input />
+            </Item>
+            <Item floatingLabel>
+              <Label>Distance</Label>
+              <Input />
+            </Item>
+            <Item floatingLabel>
+              <Label>Walk</Label>
+              <Input />
+            </Item>
+            <Item floatingLabel>
+              <Label>Power Walk</Label>
+              <Input />
+            </Item>
+            <Item floatingLabel last>
+              <Label>Run</Label>
+              <Input />
+            </Item>
+          </Form>
+        </Content>
+        <Footer>
+          <FooterTab>
+            <Button>
+              <Icon name="apps" />
+            </Button>
+            <Button>
+              <Icon name="camera" />
+            </Button>
+            <Button active>
+              <Icon active name="navigate" />
+            </Button>
+            <Button>
+              <Icon name="person" />
+            </Button>
+          </FooterTab>
+        </Footer>
+      </Container>
     )
   }
 }
