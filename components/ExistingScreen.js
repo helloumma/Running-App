57f0b69ee0134d7b2520d46dde447dd4cf5782f1
756D6D61 <756D6D61@gmail.com>
2019-09-05 15:18:59 +0100

    Existing Routines screen styled.

--- components/ExistingScreen.js
+++ components/ExistingScreen.js
@@ -4,32 +4,32 @@ import { Container, Content, Button, Text } from 'native-base';
 export default class Existingscreen extends Component {
   render() {
     return (
-        <Container>
-        <Text>Existing Routines</Text>
+        <Container style={{flex: 1,backgroundColor: '#A2FFAA', justifyContent: 'center'}}>
+        <Text style={{fontSize:'90%', textAlign: 'center', color:'#00C7AF', fontWeight: 'bold'}}>Existing Routines</Text>
         <Content>
-            <Button>
-                <Text>Routine</Text>
+            <Button style={{padding:'35%', backgroundColor:'#07B162'}}>
+                <Text style={{fontSize:'20%', textAlign: 'center', color:'white', fontWeight: 'bold'}}>Routine</Text>
             </Button>
-            <Button>
-                <Text>Routine</Text>
+            <Button style={{padding:'35%', backgroundColor:'#07B162'}}>
+                <Text style={{fontSize:'20%', textAlign: 'center', color:'white', fontWeight: 'bold'}}>Routine</Text>
             </Button>
-            <Button>
-                <Text>Routine</Text>
+            <Button style={{padding:'35%', backgroundColor:'#07B162'}}>
+                <Text style={{fontSize:'20%', textAlign: 'center', color:'white', fontWeight: 'bold'}}>Routine</Text>
             </Button>
-            <Button>
-                <Text>Routine</Text>
+            <Button style={{padding:'35%', backgroundColor:'#07B162'}}>
+                <Text style={{fontSize:'20%', textAlign: 'center', color:'white', fontWeight: 'bold'}}>Routine</Text>
             </Button>
-            <Button>
-                <Text>Routine</Text>
+            <Button style={{padding:'35%', backgroundColor:'#07B162'}}>
+                <Text style={{fontSize:'20%', textAlign: 'center', color:'white', fontWeight: 'bold'}}>Routine</Text>
             </Button>
-            <Button>
-                <Text>Routine</Text>
+            <Button style={{padding:'35%', backgroundColor:'#07B162'}}>
+                <Text style={{fontSize:'20%', textAlign: 'center', color:'white', fontWeight: 'bold'}}>Routine</Text>
             </Button>
-            <Button>
-                <Text>Routine</Text>
+            <Button style={{padding:'35%', backgroundColor:'#07B162'}}>
+                <Text style={{fontSize:'20%', textAlign: 'center', color:'white', fontWeight: 'bold'}}>Routine</Text>
             </Button>
-            <Button>
-                <Text>Routine</Text>
+            <Button style={{padding:'35%', backgroundColor:'#07B162'}}>
+                <Text style={{fontSize:'20%', textAlign: 'center', color:'white', fontWeight: 'bold'}}>Routine</Text>
             </Button>
         </Content>
     </Container>
