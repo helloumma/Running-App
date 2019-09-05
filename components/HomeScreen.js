06023f3b34231863bb4efd4342b314c39b00c032
756D6D61 <756D6D61@gmail.com>
2019-09-05 11:51:40 +0100

    New style packaged installed + buttons changed.

--- components/HomeScreen.js
+++ components/HomeScreen.js
@@ -1,26 +1,36 @@
 import React, { Component } from 'react';
-import { Button, View, Text } from 'react-native';
-import { createAppContainer } from 'react-navigation';
-import { createStackNavigator } from 'react-navigation-stack';
+import { Container, Content, Button, Text, Header } from 'native-base';
+
+
 export default class Homescreen extends Component {
   render() {
     return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Home Screen</Text>
-      
-        <Button
-            title="Existing Routine"
-            onPress={() => this.props.navigation.navigate('Existing')}
-        />
-        <Button
+        <Container>
+            <Header />
+            <Text>Home Screen</Text>
+            <Content>
+                <Button onPress={() => this.props.navigation.navigate('New')}>
+                    <Text>New Routine</Text>
+                </Button>
+                
+                <Button onPress={() => this.props.navigation.navigate('Existing')}>
+                    <Text>Existing Routine</Text>
+                </Button>
+
+                <Button onPress={() => this.props.navigation.navigate('Previous')}>
+                    <Text>Previous Runs</Text>
+                </Button>
+            </Content>
+        </Container>
+    )
+  }
+}
+/*
+<Button
             title="New Routine"
             onPress={() => this.props.navigation.navigate('New')}
         />
         <Button
             title="Previous Runs"
             onPress={() => this.props.navigation.navigate('Previous')}
-        />
-      </View>
-    )
-  }
-}
+        />*/
