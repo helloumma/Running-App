e14b4421ac5ba004fec6b12a8b7e0fac10b8bebd
756D6D61 <756D6D61@gmail.com>
2019-09-05 15:08:51 +0100

    Home screen styling.

--- components/HomeScreen.js
+++ components/HomeScreen.js
@@ -4,19 +4,19 @@ import { Container, Content, Button, Text } from 'native-base';
 export default class Homescreen extends Component {
   render() {
     return (
-        <Container>
-            <Text>Home Screen</Text>
+        <Container style={{flex: 1,backgroundColor: '#A2FFAA', justifyContent: 'center'}}>
+            <Text style={{fontSize:'100%', textAlign: 'center', color:'#00C7AF', fontWeight: 'bold'}}>START THAT RUN</Text>
             <Content>
-                <Button onPress={() => this.props.navigation.navigate('New')}>
-                    <Text>New Routine</Text>
+                <Button style={{padding:'30%', backgroundColor:'#07B162'}} onPress={() => this.props.navigation.navigate('New')}>
+                    <Text style={{fontSize:'20%', textAlign: 'center', color:'white', fontWeight: 'bold'}}>New Routine</Text>
                 </Button>
                 
-                <Button onPress={() => this.props.navigation.navigate('Existing')}>
-                    <Text>Existing Routine</Text>
+                <Button style={{padding:'25%', backgroundColor:'#07B162'}} onPress={() => this.props.navigation.navigate('Existing')}>
+                    <Text style={{fontSize:'20%', textAlign: 'center', color:'white', fontWeight: 'bold'}}>Existing Routine</Text>
                 </Button>
 
-                <Button onPress={() => this.props.navigation.navigate('Previous')}>
-                    <Text>Previous Runs</Text>
+                <Button style={{padding:'30%', backgroundColor:'#07B162'}} onPress={() => this.props.navigation.navigate('Previous')}>
+                    <Text style={{fontSize:'20%', textAlign: 'center', color:'white', fontWeight: 'bold'}}>Previous Runs</Text>
                 </Button>
             </Content>
         </Container>
