44beabb8f35915f14021a9ee2ddabe5f0812dd3d
756D6D61 <756D6D61@gmail.com>
2019-09-05 15:28:22 +0100

    Previous runs styling.

--- components/PreviousScreen.js
+++ components/PreviousScreen.js
@@ -4,32 +4,33 @@ import { Container, Content, Button, Text } from 'native-base';
 export default class Previousscreen extends Component {
   render() {
     return (
-        <Container>
-        <Text>Previous Runs</Text>
+        <Container style={{flex: 1,backgroundColor: '#A2FFAA', justifyContent: 'center'}}>
+        <Text style={{fontSize:40, textAlign: 'center', color:'#00C7AF', fontWeight: 'bold'}}>START THAT RUN</Text>
+        <Text style={{fontSize:40, textAlign: 'center', color:'#07B162', fontWeight: 'bold'}}>Previous Runs</Text>
         <Content>
-            <Button>
-                <Text>Date</Text>
+        <Button style={{padding:'40%', backgroundColor:'#07B162'}}>
+                <Text style={{fontSize:20, textAlign: 'center', color:'white', fontWeight: 'bold'}}>Date</Text>
             </Button>
-            <Button>
-                <Text>Date</Text>
+            <Button style={{padding:'40%', backgroundColor:'#07B162'}}>
+                <Text style={{fontSize:20, textAlign: 'center', color:'white', fontWeight: 'bold'}}>Date</Text>
             </Button>
-            <Button>
-                <Text>Date</Text>
+            <Button style={{padding:'40%', backgroundColor:'#07B162'}}>
+                <Text style={{fontSize:20, textAlign: 'center', color:'white', fontWeight: 'bold'}}>Date</Text>
             </Button>
-            <Button>
-                <Text>Date</Text>
+            <Button style={{padding:'40%', backgroundColor:'#07B162'}}>
+                <Text style={{fontSize:20, textAlign: 'center', color:'white', fontWeight: 'bold'}}>Date</Text>
             </Button>
-            <Button>
-                <Text>Date</Text>
+            <Button style={{padding:'40%', backgroundColor:'#07B162'}}>
+                <Text style={{fontSize:20, textAlign: 'center', color:'white', fontWeight: 'bold'}}>Date</Text>
             </Button>
-            <Button>
-                <Text>Date</Text>
+            <Button style={{padding:'40%', backgroundColor:'#07B162'}}>
+                <Text style={{fontSize:20, textAlign: 'center', color:'white', fontWeight: 'bold'}}>Date</Text>
             </Button>
-            <Button>
-                <Text>Date</Text>
+            <Button style={{padding:'40%', backgroundColor:'#07B162'}}>
+                <Text style={{fontSize:20, textAlign: 'center', color:'white', fontWeight: 'bold'}}>Date</Text>
             </Button>
-            <Button>
-                <Text>New Routine</Text>
+            <Button style={{padding:'40%', backgroundColor:'#07B162'}}>
+                <Text style={{fontSize:20, textAlign: 'center', color:'white', fontWeight: 'bold'}}>Date</Text>
             </Button>
         </Content>
     </Container>
