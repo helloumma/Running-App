9699f2252a502aadbbfcb93fc6a7d42d2bb41c62
756D6D61 <756D6D61@gmail.com>
2019-09-11 10:39:43 +0100

    Edit runs: border/background/blocks

--- components/PreviousScreen.js
+++ components/PreviousScreen.js
@@ -1,39 +1,121 @@
 import React, { Component } from 'react';
-import { Container, Content, Button, Text } from 'native-base';
+import { Container, Content, Button } from 'native-base';
+import { StyleSheet, View, Text } from 'react-native';
+import * as Font from 'expo-font'
+import HomeSvg from './Home/HomeIcon'
+import Image from 'react-native-svg'
+import testSvg from '../assets/Home/Home.svg';
 
 export default class Previousscreen extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+          fontLoaded: false
+        }
+    }
+      async componentDidMount() {
+        await Font.loadAsync({
+            test1: require('../assets/fonts/SourceCodePro-Medium.ttf'),
+            test2: require('../assets/fonts/SourceCodePro-Light.ttf'),
+        })
+        this.setState({fontLoaded: true})
+     }
+     
   render() {
+    if (!this.state.fontLoaded) {
+        return <Text>False</Text>
+      }
     return (
-        <Container style={{flex: 1,backgroundColor: '#A2FFAA', justifyContent: 'center'}}>
-        <Text style={{fontSize:40, textAlign: 'center', color:'#00C7AF', fontWeight: 'bold'}}>START THAT RUN</Text>
-        <Text style={{fontSize:40, textAlign: 'center', color:'#07B162', fontWeight: 'bold'}}>Previous Runs</Text>
-        <Content>
-        <Button style={{padding:'40%', backgroundColor:'#07B162'}}>
-                <Text style={{fontSize:20, textAlign: 'center', color:'white', fontWeight: 'bold'}}>Date</Text>
-            </Button>
-            <Button style={{padding:'40%', backgroundColor:'#07B162'}}>
-                <Text style={{fontSize:20, textAlign: 'center', color:'white', fontWeight: 'bold'}}>Date</Text>
-            </Button>
-            <Button style={{padding:'40%', backgroundColor:'#07B162'}}>
-                <Text style={{fontSize:20, textAlign: 'center', color:'white', fontWeight: 'bold'}}>Date</Text>
-            </Button>
-            <Button style={{padding:'40%', backgroundColor:'#07B162'}}>
-                <Text style={{fontSize:20, textAlign: 'center', color:'white', fontWeight: 'bold'}}>Date</Text>
-            </Button>
-            <Button style={{padding:'40%', backgroundColor:'#07B162'}}>
-                <Text style={{fontSize:20, textAlign: 'center', color:'white', fontWeight: 'bold'}}>Date</Text>
-            </Button>
-            <Button style={{padding:'40%', backgroundColor:'#07B162'}}>
-                <Text style={{fontSize:20, textAlign: 'center', color:'white', fontWeight: 'bold'}}>Date</Text>
-            </Button>
-            <Button style={{padding:'40%', backgroundColor:'#07B162'}}>
-                <Text style={{fontSize:20, textAlign: 'center', color:'white', fontWeight: 'bold'}}>Date</Text>
-            </Button>
-            <Button style={{padding:'40%', backgroundColor:'#07B162'}}>
-                <Text style={{fontSize:20, textAlign: 'center', color:'white', fontWeight: 'bold'}}>Date</Text>
-            </Button>
-        </Content>
-    </Container>
+        <View style={styles.container}>
+            <Content style={styles.blueBox}></Content>
+            <Content style={styles.greenBox}></Content>
+            <Content style={styles.content}>
+            <Text style={styles.title}>View Run</Text>
+            <Text style={styles.navText}>Home</Text>
+            <Content style={styles.imageContent}>
+            <Image
+                source={{ uri: '../assets/Home/Home.svg' }}
+                style={{ width: 100, height: 100}}
+            />
+            </Content>
+            <Text style={styles.navText} onPress={() => this.props.navigation.navigate('New')}>Add</Text>
+            <Text style={styles.navText} onPress={() => this.props.navigation.navigate('Existing')}>Edit</Text>
+            <Text style={styles.navText} onPress={() => this.props.navigation.navigate('Previous')}>View</Text>
+            <Text style={styles.navText} onPress={() => this.props.navigation.navigate('Route')}>Route</Text>
+            </Content>
+        </View>
     )
   }
 }
+
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: '#C0A4F2',
+        justifyContent: 'center',
+        flex: 1,
+    }, 
+    content: {
+        position: 'absolute',
+        borderWidth: 1,
+        borderColor: '#fff',
+        top: -425,
+        left:30,
+        right: 30,
+        borderRadius:  8,
+        backgroundColor: 'transparent',
+        height: 675,
+        zIndex:-1
+    },
+    imageContent: {
+        position: 'absolute',
+        top: 75,
+        left: 160,
+        zIndex: 2
+    },
+    blueBox: {
+        position: 'absolute',
+        top: 60,
+        left: 60,
+        height: 625, 
+        width: 600,
+        backgroundColor: '#A8CFE3',
+        zIndex:0
+    },
+    greenBox: {
+        position: 'absolute',
+        top: -130,
+        left: -70,
+        height: 120, 
+        width: 400,
+        backgroundColor: '#00F6B7',
+        zIndex:1
+    },
+    home: {
+        fontSize: 20, 
+        textAlign: 'center', 
+        color:'white', 
+        fontWeight: 'bold',
+        fontFamily: 'test1'
+        },
+    title: {
+        fontSize:66, 
+        textAlign: 'left', 
+        color:'white', 
+        fontWeight: 'bold',
+        fontFamily: 'test1',
+        paddingTop: 70,
+        zIndex: 1,
+        left:10,
+        top: 10
+    }, 
+    navText: {
+        fontSize:30, 
+        textAlign: 'left', 
+        color:'white', 
+        fontWeight: 'bold',
+        fontFamily: 'test2',
+        paddingTop: 40,
+        paddingLeft: '50%',
+        zIndex: 1,
+    }
+})
