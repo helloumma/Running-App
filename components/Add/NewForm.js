777ff2ba46f5ac00452123ac70f43e51388f5ea5
756D6D61 <756D6D61@gmail.com>
2019-09-11 13:06:54 +0100

    Form styling

--- components/Add/NewForm.js
+++ components/Add/NewForm.js
@@ -1,16 +1,39 @@
 import React, { Component } from 'react';
-import { Container, Header, Content, Form, Item, Input, Label } from 'native-base';
+import { StyleSheet, Text } from 'react-native';
+import { Content, Form, Item, Input, Label } from 'native-base';
+import * as Font from 'expo-font'
+
 export default class NewForm extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      fontLoaded: false
+    }
+}
+  async componentDidMount() {
+    await Font.loadAsync({
+        test1: require('../../assets/fonts/SourceCodePro-Medium.ttf'),
+        test2: require('../../assets/fonts/SourceCodePro-Light.ttf'),
+    })
+    this.setState({fontLoaded: true})
+ }
   render() {
+    if (!this.state.fontLoaded) {
+      return <Text>False</Text>
+    }
     return (
         <Content>
           <Form>
-            <Item stackedLabel>
-              <Label>Username</Label>
+            <Item stackedLabel >
+              <Label style={styles.label}>Name</Label>
               <Input />
             </Item>
-            <Item stackedLabel last>
-              <Label>Password</Label>
+            <Item stackedLabel>
+              <Label style={styles.label}>Time</Label>
+              <Input />
+            </Item>
+            <Item stackedLabel>
+              <Label style={styles.label}>Distance</Label>
               <Input />
             </Item>
           </Form>
@@ -18,3 +41,13 @@ export default class NewForm extends Component {
     );
   }
 }
+
+const styles = StyleSheet.create({
+  label: {
+      fontSize:20, 
+      textAlign: 'left', 
+      color:'white', 
+      fontFamily: 'test2',
+  }, 
+  
+})
